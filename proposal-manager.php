b443963147d231dcd2726edd89f9436612d7643b
Michael Wender <michael@michaelwender.com>
2018-01-29 10:16:54 -0500

    First commit

---
+++ proposal-manager.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Hollingsworth Proposal Manager
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Adds a Proposal CPT for quick and easy creation of Hollingsworth Construction proposals.
+ * Author:          Michael Wender
+ * Author URI:      https://mwender.com
+ * Text Domain:     proposal-manager
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Proposal_Manager
+ */
