41ccb390d98c1c0f05420fd853d8160be642b6b0
Michael Wender <michael@michaelwender.com>
2018-02-09 14:54:16 -0500

    Moving `About Hollingsworth` into column next to video

--- templates/proposal.php
+++ templates/proposal.php
@@ -35,57 +35,54 @@
         </div>
       </section>
     </nav>
-    <section class="container" id="video"<?php if( 'yes' == $video_viewed ) echo ' style="display: none;"' ?>>
-      <div class="video-container">
-        <div id="player"></div>
-      </div>
-    </section>
-    <section class="container" id="attachments" style="<?php if( 'yes' != $video_viewed ) echo 'display: none;' ?>">
+    <section class="container" id="videox">
       <div class="row">
-        <div class="column column-60">
-          <h3>Your proposal package:</h3>
-          <?php
-          $pdf_query_args = [
-            'post_type' => 'attachment',
-            'post_mime_type' => 'application/pdf',
-            'post_status' => 'inherit',
-            'posts_per_page' => -1,
-            'post_parent' => $post->ID
-          ];
-          $pdf_query = new WP_Query( $pdf_query_args );
-          if( $pdf_query->have_posts() ):
-            echo '<ul class="lrg">';
-            while( $pdf_query->have_posts() ){
-              $pdf_query->the_post();
-              echo '<li><a href="' . wp_get_attachment_url( get_the_ID() ) . '" target="_blank">' . get_the_title() . '</a></li>';
-            }
-            echo '</ul>';
-            wp_reset_postdata();
-          else:
-            echo '<p>No files have been included.</p>';
-          endif;
-          ?>
+        <div class="column column-67">
+          <div id="video"<?php if( 'yes' == $video_viewed ) echo ' style="display: none;"' ?>>
+            <h3 style="margin-bottom: 1.4rem;">Introductory Video</h3>
+            <div class="video-container">
+              <div id="player"></div>
+            </div>
+          </div>
+          <div id="attachments" <?php if( 'yes' != $video_viewed ) echo 'style="display: none;"' ?>>
+            <h3>Your proposal package:</h3>
+            <?php
+            $pdf_query_args = [
+              'post_type' => 'attachment',
+              'post_mime_type' => 'application/pdf',
+              'post_status' => 'inherit',
+              'posts_per_page' => -1,
+              'post_parent' => $post->ID
+            ];
+            $pdf_query = new WP_Query( $pdf_query_args );
+            if( $pdf_query->have_posts() ):
+              echo '<ul class="lrg">';
+              while( $pdf_query->have_posts() ){
+                $pdf_query->the_post();
+                echo '<li><a href="' . wp_get_attachment_url( get_the_ID() ) . '" target="_blank">' . get_the_title() . '</a></li>';
+              }
+              echo '</ul>';
+              wp_reset_postdata();
+            else:
+              echo '<p>No files have been included.</p>';
+            endif;
+
+            $post_content = apply_filters( 'the_content', get_the_content() );
+            if( ! empty( $post_content ) )
+              echo '<hr/><h4>Notes</h4>' . $post_content;
+            ?>
+          </div>
         </div>
-        <div class="column column-40">
-          <?php
-          $post_content = apply_filters( 'the_content', get_the_content() );
-          if( ! empty( $post_content ) )
-            echo '<h4>Notes</h4>' . $post_content;
-          ?>
-        </div>
-      </div>
-    </section>
-    <section class="container grey" id="about">
-      <h4>About The Hollingsworth Companies</h4>
-      <div class="row">
-        <div class="column column-25">
+        <div class="column column-33" id="about">
+          <h4>About The Hollingsworth Companies</h4>
           <a href="<?php echo site_url('our-listings') ?>" target="_blank"><img class="border" src="<?php echo HPM_PLUGIN_DIR_URL; ?>lib/img/map-cropped.jpg" /></a>
-        </div>
-        <div class="column column-75">
-          <p><strong>Building on Success</strong><br/>The Hollingsworth Companies Industrial Building Program doesn’t just build industrial facilities. We build businesses. Because we are likely to own all the real estate surrounding any of our facilities that you might select, we take a very serious interest in making sure your business is successful and growing. Our business grows when your business thrives, so it is only natural for us to want to see you do well. The list of creative solutions to business challenges is as long as our 50 years of combined experience, and it will continue to get longer.</p>
+          <p class="hidetext"><strong>Building on Success: </strong><br/>The Hollingsworth Companies Industrial Building Program doesn’t just build industrial facilities. We build businesses. Because we are likely to own all the real estate surrounding any of our facilities that you might select, we take a very serious interest in making sure your business is successful and growing. Our business grows when your business thrives, so it is only natural for us to want to see you do well. The list of creative solutions to business challenges is as long as our 50 years of combined experience, and it will continue to get longer.</p>
         </div>
       </div>
-      <div class="row">
+    </section>
+    <section class="container grey" id="view-properties">
+      <h4>View Our Properties</h4>
+      <div class="row" style="margin-bottom: 10px;">
         <div class="column">
           <div class="card">
             <div class="card-image"><a href="<?php echo site_url('our-listings?view=5') ?>" target="_blank"><img src="<?php echo HPM_PLUGIN_DIR_URL ?>lib/img/click-to-find-properties.jpg" /></a></div>
@@ -97,26 +94,23 @@
             <div class="card-image"><a href="<?php echo site_url('our-listings?view=1') ?>" target="_blank"><img src="<?php echo HPM_PLUGIN_DIR_URL ?>lib/img/available-now.jpg" /></a></div>
             <div class="card-caption"><a href="<?php echo site_url('our-listings?view=1') ?>" target="_blank">Available Now</a></div>
           </div>
-
         </div>
         <div class="column">
           <div class="card">
             <div class="card-image"><a href="<?php echo site_url('our-listings?view=2') ?>" target="_blank"><img src="<?php echo HPM_PLUGIN_DIR_URL ?>lib/img/construction-ready.jpg" /></a></div>
             <div class="card-caption"><a href="<?php echo site_url('our-listings?view=2') ?>" target="_blank">Under Construction Ready in 120 Days</a></div>
           </div>
-
         </div>
         <div class="column">
           <div class="card">
             <div class="card-image"><a href="<?php echo site_url('our-listings?view=3') ?>" target="_blank"><img src="<?php echo HPM_PLUGIN_DIR_URL ?>lib/img/build-to-suit.jpg" /></a></div>
             <div class="card-caption"><a href="<?php echo site_url('our-listings?view=3') ?>" target="_blank">Build to Suit in as little as 180 days</a></div>
           </div>
-
         </div>
       </div>
     </section>
     <footer class="footer">
-      <section class="container"><p>&copy; <?php echo date('Y') ?> <?php bloginfo( 'name' ) ?>. All rights reserved.<br/>2 Centre Plaza, Clinton, TN 37716, (865) 457-3601.</p></section>
+      <section class="container"><p>&copy; <?php echo date('Y') ?> All rights reserved. <?php bloginfo( 'name' ) ?>, 2 Centre Plaza, Clinton, TN 37716, (865) 457-3601.</p></section>
     </footer>
   </main>
   <?php wp_footer() ?>
