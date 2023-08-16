ba15e29bf9f752310f95dc00c094401ec3ddaefb
Michael Wender <michael@michaelwender.com>
2023-08-16 14:06:03 -0400

    Removing unused code

--- proposal-manager.php
+++ proposal-manager.php
@@ -17,20 +17,6 @@ define( 'HPM_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
 // Initialize TypeRocket
 require ( 'lib/typerocket/init.php' );
 
-// Initialize Plugin Updates
-/*
-require_once ( plugin_dir_path( __FILE__ ) . 'lib/classes/plugin-updater.php' );
-if( is_admin() ){
-    add_action( 'init', function(){
-        // If you're experiencing GitHub API rate limits while testing
-        // plugin updates, create a `Personal access token` under your
-        // GitHub profile's `Developer Settings`. Then add
-        // `define( 'GITHUB_ACCESS_TOKEN', your_access_token );` to
-        // your site's `wp-config.php`.
-        new GitHub_Plugin_Updater( __FILE__, 'mwender', 'hollingsworth-proposal-manager', GITHUB_ACCESS_TOKEN );
-    } );
-}
-/**/
 function hide_adminbar_on_proposals(){
     if(
       is_singular( 'proposal' )
