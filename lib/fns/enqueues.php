8968666e6ef2d01cc30eac808a0efe26f2a10ba0
Michael Wender <michael@michaelwender.com>
2024-04-11 14:34:35 -0400

    Adding jQuery as required dependency

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -27,8 +27,8 @@ function enqueue_scripts(){
     //wp_register_script( 'youtube-api', 'https://www.youtube.com/iframe_api' );
     wp_enqueue_style( 'video-js', 'https://vjs.zencdn.net/7.4.1/video-js.css', null, '7.4.1' );
     //wp_register_script( 'video-js', 'https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js', null, '1.1.2' );
-    wp_register_script( 'video-js', HPM_PLUGIN_DIR_URL . 'lib/js/video.min.js', null, '7.3.0' );
-    wp_enqueue_script( 'proposal', HPM_PLUGIN_DIR_URL . 'lib/js/proposal.js', ['js-cookie','video-js'], filemtime( HPM_PLUGIN_DIR_PATH . 'lib/js/proposal.js'), true );
+    wp_register_script( 'video-js', HPM_PLUGIN_DIR_URL . 'lib/js/video.min.js', ['jquery'], '7.3.0' );
+    wp_enqueue_script( 'proposal', HPM_PLUGIN_DIR_URL . 'lib/js/proposal.js', ['jquery','js-cookie','video-js'], filemtime( HPM_PLUGIN_DIR_PATH . 'lib/js/proposal.js'), true );
     $video = get_post_meta( $post->ID, 'video', true );
     $poster_frame = wp_get_attachment_url( 2342 );
     wp_localize_script( 'proposal', 'scriptvars', [
